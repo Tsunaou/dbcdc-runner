e3e2a2d4b582dc87113646aebb1ca6647fd68597
Hengfeng Wei <hfwei@nju.edu.cn>
2024-10-12 01:02:20 +0800

    db-config.edn; run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 lein run test-all -w rw \
---max-writes-per-key 4 \
---concurrency 50 \
+--key-count 100 \
+--max-writes-per-key 100 \
+--concurrency 10 \
+--txn-num 1000 \
 -r 500 \
+--time-limit 1440000 \
+--nemesis none \
+--existing-postgres \
 --node dummy-node \
 --isolation serializable \
 --expected-consistency-model serializable \
---time-limit 10000 \
---nemesis none \
---existing-postgres \
 --no-ssh \
 --database postgresql \
+
 # --varchar-table
+
+lein run test-all -w rw \
+--key-count 100 \
+--max-writes-per-key 100 \
+--concurrency 10 \
+--txn-num 1000 \
+-r 500 \
+--time-limit 1440000 \
+--nemesis none \
+--existing-postgres \
+--node dummy-node \
+--isolation serializable \
+--expected-consistency-model serializable \
+--no-ssh \
+--database postgresql \
