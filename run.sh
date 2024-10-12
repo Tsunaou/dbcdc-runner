f45dbf469198a3604583417a8187779400c5e5f1
Hengfeng Wei <hfwei@nju.edu.cn>
2024-10-12 11:27:41 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
+
 lein run test-all -w rw \
 --key-count 100 \
 --max-writes-per-key 100 \
 --concurrency 10 \
---txn-num 1000 \
--r 500 \
+--rate 500 \
 --time-limit 1440000 \
+--txn-num 1000 \
+--test-count 100
+--max-txn-length 4 \
 --nemesis none \
 --existing-postgres \
 --node dummy-node \
 --isolation serializable \
 --expected-consistency-model serializable \
 --no-ssh \
---database postgresql \
-
+--database postgresql
 # --varchar-table
-
-lein run test-all -w rw \
---key-count 100 \
---max-writes-per-key 100 \
---concurrency 10 \
---txn-num 1000 \
--r 500 \
---time-limit 1440000 \
---nemesis none \
---existing-postgres \
---node dummy-node \
---isolation serializable \
---expected-consistency-model serializable \
---no-ssh \
---database postgresql \
